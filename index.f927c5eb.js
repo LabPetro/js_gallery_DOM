3924e6ada2918a6de707fbd8c7950eb23b5f4aab
LabPetro <lamppetro30@outlook.com>
2024-01-22 02:08:00 +0200

    make build

---
+++ index.f927c5eb.js
@@ -0,0 +1,3 @@
+var t=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var a="img"===e.target.tagName.toLowerCase()?e.target.parentNode:e.target;if(a.classList.contains("list-item__link")){var r=a.getAttribute("href");t.setAttribute("src",r)}});//# sourceMappingURL=index.f927c5eb.js.map
+
+//# sourceMappingURL=index.f927c5eb.js.map
